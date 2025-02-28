7a4d9a637f4dacd366a509139c58f49d509030be
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-02-28 15:47:54 +0100

    finished photoaxis.lua

--- hellorobot/phototaxis.lua
+++ hellorobot/phototaxis.lua
@@ -1,20 +1,8 @@
 -- Put your global variables here
 
-MOVE_STEPS = 15
-MAX_VELOCITY = 10
-LIGHT_THRESHOLD = 1.5
-
-n_steps = 0
-
-
 --[[ This function is executed every time you press the 'execute'
      button ]]
 function init()
-	left_v = robot.random.uniform(0,MAX_VELOCITY)
-	right_v = robot.random.uniform(0,MAX_VELOCITY)
-	robot.wheels.set_velocity(left_v,right_v)
-	n_steps = 0
-	robot.leds.set_all_colors("black")
 end
 
 
@@ -22,64 +10,35 @@ end
 --[[ This function is executed at each time step
      It must contain the logic of your controller ]]
 function step()
-	n_steps = n_steps + 1
-	if n_steps % MOVE_STEPS == 0 then
-		left_v = robot.random.uniform(0,MAX_VELOCITY)
-		right_v = robot.random.uniform(0,MAX_VELOCITY)
+-- ---------------------------- BEST SOLUTION UP TO NOW
+	sens1 = robot.light[1].value
+	sens24 = robot.light[24].value
+	log("01: " .. sens1)
+	log("24: " .. sens24)
+	avg = (sens1 + sens24) / 2
+	correction = (1 - avg) * 5
+	right_velocity = 0
+	left_velocity = 0
+	if sens1 > sens24 then
+		right_velocity = right_velocity + correction
+	else 
+		left_velocity = left_velocity + correction
 	end
-	robot.wheels.set_velocity(left_v,right_v)
-	log("robot.position.x = " .. robot.positioning.position.x)
-	log("robot.position.y = " .. robot.positioning.position.y)
-	log("robot.position.z = " .. robot.positioning.position.z)
-	light_front = robot.light[1].value + robot.light[24].value
-	log("robot.light_front = " .. light_front)
-	ground = robot.motor_ground --just to save some chars...
-	log("ground NW: " .. ground[1].value)
-	log("ground SW: " .. ground[2].value)
-	log("ground SE: " .. ground[3].value)
-	log("ground NE: " .. ground[4].value)
+	robot.wheels.set_velocity(5 + left_velocity, 5 + right_velocity)
 	
-	-- Search for the reading with the highest value
-	value = -1 -- highest value found so far
-	idx = -1   -- index of the highest value
-	for i=1,#robot.proximity do
-		if value < robot.proximity[i].value then
-			idx = i
-			value = robot.proximity[i].value
-		end
-	end
-	log("robot max proximity sensor: " .. idx .. "," .. value)
+-- ----------------------------
 
-	-- Check if on spot
-	spot = false
-	for i=1,4 do
-		if ground[i].value == 0 then
-			spot = true
-			break
-		end
-	end
+	-- sens1 = robot.light[1].value
+	-- sens24 = robot.light[24].value
 
-
-	--[[ Check if close to light 
-	(note that the light threshold depends on both sensor and actuator characteristics) ]]
-	light = false
-	sum = 0
-	for i=1,#robot.light do
-		sum = sum + robot.light[i].value
-	end
-	if sum > LIGHT_THRESHOLD then
-		light = true
-	end
-
-
-	if spot == true then
-		robot.leds.set_all_colors("red")
-	elseif light == true then
-		robot.leds.set_all_colors("yellow")
-	else
-		robot.leds.set_all_colors("black")
-	end
-
+	-- left_velocity = 0
+	-- right_velocity = 0
+	-- if sens1 > sens24 then
+	-- 	right_velocity = right_velocity + 1
+	-- else 
+	-- 	left_velocity = left_velocity + 1
+	-- end
+	-- robot.wheels.set_velocity(5 + left_velocity, 5 + right_velocity)
 
 end
 
@@ -91,11 +50,8 @@ end
      called. The state of sensors and actuators is reset
      automatically by ARGoS. ]]
 function reset()
-	left_v = robot.random.uniform(0,MAX_VELOCITY)
-	right_v = robot.random.uniform(0,MAX_VELOCITY)
-	robot.wheels.set_velocity(left_v,right_v)
+	robot.wheels.set_velocity(0,0)
 	n_steps = 0
-	robot.leds.set_all_colors("black")
 end
 
 
