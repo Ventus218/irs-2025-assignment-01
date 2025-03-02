d4a723729ce2044eb065910c1513609bd3484ab9
Alessandro Venturini <61183738+Ventus218@users.noreply.github.com>
2025-03-02 10:55:32 +0100

    adjusted code of phototaxis

--- simulator/phototaxis.lua
+++ simulator/phototaxis.lua
@@ -1,24 +1,24 @@
 
+VELOCITY = 15
+
 function init()
 	reset()
 end
 
 function step()
 -- ---------------------------- BEST SOLUTION UP TO NOW
-	sens1 = robot.light[1].value
-	sens24 = robot.light[24].value
-	log("01: " .. sens1)
-	log("24: " .. sens24)
-	avg = (sens1 + sens24) / 2
-	correction = (1 - avg) * 5
-	right_velocity = 0
-	left_velocity = 0
-	if sens1 > sens24 then
-		right_velocity = right_velocity + correction
+	sens_l = robot.light[3].value
+	sens_r = robot.light[22].value
+	log("L: " .. sens_l)
+	log("R: " .. sens_r)
+	avg = (sens_l + sens_r) / 2
+	diff = math.abs(sens_l - sens_r)
+	correction = diff * 50
+	if (sens_l >= sens_r) or ((sens_l == 0) and (sens_r == 0))  then
+		robot.wheels.set_velocity(VELOCITY, VELOCITY + correction)
 	else 
-		left_velocity = left_velocity + correction
+		robot.wheels.set_velocity(VELOCITY + correction, VELOCITY)
 	end
-	robot.wheels.set_velocity(5 + left_velocity, 5 + right_velocity)
 	
 -- ----------------------------
 
